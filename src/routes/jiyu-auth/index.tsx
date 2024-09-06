22807bd06b766ff0bf0bb855caa964bbc4e68f55
MochaLatte <20302010067@fudan.edu.cn>
2024-09-06 21:39:10 +0800

    feat: uis 跳转小程序逻辑

--- src/routes/jiyu-auth/index.tsx
+++ src/routes/jiyu-auth/index.tsx
@@ -5,9 +5,18 @@ import wx from "weixin-js-sdk";
 export const JiYuAuth = (): JSX.Element => {
   useEffect(() => {
     if (isWx()) {
-      wx.miniProgram.postMessage({ data: "foo" });
+      const url = window.location.href;
+      const urlObj = new URL(url);
+      const params = new URLSearchParams(urlObj.search);
+      const code = params.get("code");
+      const scope = params.get("scope");
+      const state = params.get("state");
+      const clientId = params.get("client_id");
       // 一定要销毁 webview 才能收到消息
-      wx.miniProgram.switchTab({ url: "/pages/home/index" });
+      // 需要特别注意，得先返回再去postMessage（逆天啊逆天）
+      wx.miniProgram.navigateBack({ url: "/pages/auth/index" }); // 官方文档不规范的典范 ts 里 params 写着 url 文档里写的 delta
+      // 我问问你腾讯你做这个小程序有没有用心在写 这b玩意儿谁开发谁傻逼
+      wx.miniProgram.postMessage({ data: { code, scope, state, clientId } });
     }
   }, []);
 
